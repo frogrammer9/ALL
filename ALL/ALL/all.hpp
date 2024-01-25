d961f54c0db946b1af6fce680ea995ea54ef0e44
frogrammer9 <oskar@meler.pl>
2024-01-25 20:24:50 +0100

    Implemented basic logging funcions

--- ALL/ALL/all.hpp
+++ ALL/ALL/all.hpp
@@ -9,32 +9,72 @@
 #include<chrono>
 
 #if defined __GNUC__ || __clang__
-	#define ALL_ENT(text, ...) all::Logger::getInstance().entry(text __VA_OPT__(,) __VA_ARGS__)
-	#define ALL_INF(text, ...) all::Logger::getInstance().info(text __VA_OPT__(,) __VA_ARGS__)
-	#define ALL_WAR(text, ...) all::Logger::getInstance().warning(text __VA_OPT__(,) __VA_ARGS__)
-	#define ALL_ERR(text, ...) all::Logger::getInstance().error(text __VA_OPT__(,) __VA_ARGS__)
-
-	#define ALL_ENT(...) all::Logger::getInstance().entry(text __VA_OPT__(,) __VA_ARGS__)
-	#define ALL_INF(...) all::Logger::getInstance().info(text __VA_OPT__(,) __VA_ARGS__)
-	#define ALL_WAR(...) all::Logger::getInstance().warning(text __VA_OPT__(,) __VA_ARGS__)
-	#define ALL_ERR(...) all::Logger::getInstance().error(text __VA_OPT__(,) __VA_ARGS__)
+	#ifndef ALL_DISABLE_ENT
+		#define ALL_ENT(first, ...) all::Logger::getInstance().log(0, first, __VA_OPT__(,) __VA_ARGS__)
+	#else
+		#define ALL_ENT(...)
+	#endif
+	#ifndef ALL_DISABLE_INF
+		#define ALL_INF(first, ...) all::Logger::getInstance().log(1, first, __VA_OPT__(,) __VA_ARGS__)
+	#else
+		#define ALL_INF(...)
+	#endif
+	#ifndef ALL_DISABLE_WAR
+		#define ALL_WAR(first, ...) all::Logger::getInstance().log(2, first, __VA_OPT__(,) __VA_ARGS__)
+	#else
+		#define ALL_WAR(...)
+	#endif
+	#ifndef ALL_DISABLE_ERR
+		#define ALL_ERR(first, ...) all::Logger::getInstance().log(3, first, __VA_OPT__(,) __VA_ARGS__)
+	#else
+		#define ALL_ERR(...)
+	#endif
 #elif defined _MSC_VER
-	#define ALL_ENT(text, ...) all::Logger::getInstance().entry(text, ## __VA_ARGS__)
-	#define ALL_INF(text, ...) all::Logger::getInstance().info(text, ## __VA_ARGS__)
-	#define ALL_WAR(text, ...) all::Logger::getInstance().warning(text, ## __VA_ARGS__)
-	#define ALL_ERR(text, ...) all::Logger::getInstance().error(text, ## __VA_ARGS__)
-	
-	#define ALL_ENT(...) all::Logger::getInstance().entry(## __VA_ARGS__)
-	#define ALL_INF(...) all::Logger::getInstance().info(## __VA_ARGS__)
-	#define ALL_WAR(...) all::Logger::getInstance().warning(## __VA_ARGS__)
-	#define ALL_ERR(...) all::Logger::getInstance().error(## __VA_ARGS__)
+	#ifndef ALL_DISABLE_ENT
+		#define ALL_ENT(first, ...) all::Logger::getInstance().log(0, first, ## __VA_ARGS__)
+	#else
+		#define ALL_ENT(...)
+	#endif
+	#ifndef ALL_DISABLE_INF
+		#define ALL_INF(first, ...) all::Logger::getInstance().log(1, first, ## __VA_ARGS__)
+	#else
+		#define ALL_INF(...)
+	#endif
+	#ifndef ALL_DISABLE_WAR
+		#define ALL_WAR(first, ...) all::Logger::getInstance().log(2, first, ## __VA_ARGS__)
+	#else
+		#define ALL_WAR(...)
+	#endif
+	#ifndef ALL_DISABLE_ERR
+		#define ALL_ERR(first, ...) all::Logger::getInstance().log(3, first, ## __VA_ARGS__)
+	#else
+		#define ALL_ERR(...)
+	#endif
 #else
-	#error Supported compiler has not beed defined
+	#error This library only supports MSVC, Clang and GNU
 #endif
 
+#define ALL_LOG all::Logger::getInstance()
 
+#define ALL_BLACK			"\033[0;30m"
+#define ALL_DARK_GRAY		"\033[1;30m"
+#define ALL_BLUE			"\033[0;34m"
+#define ALL_LIGHT_BLUE		"\033[1;34m"
+#define ALL_GREEN			"\033[0;32m"
+#define ALL_LIGHT_GREEN		"\033[1;32m"
+#define ALL_CYAN			"\033[0;36m"
+#define ALL_LIGHT_CYAN		"\033[1;36m"
+#define ALL_RED				"\033[0;31m"
+#define ALL_LIGHT_RED		"\033[1;31m"
+#define ALL_PURPLE			"\033[0;35m"
+#define ALL_LIGHT_PURPLE	"\033[1;35m"
+#define ALL_BROWN			"\033[0;33m"
+#define ALL_YELLOW			"\033[1;33m"
+#define ALL_LIGHT_GRAY		"\033[0;37m"
+#define ALL_WHITE			"\033[1;37m"
+#define ALL_DEFAULT			"\0"
 
-
+#define ALL_COLOR std::string
 
 namespace all {
 class Logger
@@ -51,52 +91,77 @@ public:
 	  // string + variables
 	  //===============================================================================
 	template<typename ...Types>
-	void entry(std::string message, Types&& ... args)
+	void log(uint8_t mode, std::string first, Types&& ... args)
 	{
-
-	}
-	template<typename ...Types>
-	void info(std::string message, Types&& ... args)
-	{
-
-	}
-	template<typename ...Types>
-	void warning(std::string message, Types&& ... args)
-	{
-
-	}
-	template<typename ...Types>
-	void error(std::string message, Types&& ... args)
-	{
-
+		m_msg << m_colors[mode];
+		m_msg << getTime() << m_type[mode];
+		output(first, args...);
+		m_msg << "\033[0m"; std::cout << m_msg.str() << std::endl;
 	}
 	//===============================================================================
-	// variables only
+	// settings
 	//===============================================================================
-	template<typename ...Types>
-	void entry(Types&& ... args)
+	void setColors(ALL_COLOR entryColor, ALL_COLOR infoColor, ALL_COLOR warningColor, ALL_COLOR errorColor)
 	{
-
+		m_colors[0] = (entryColor == "\0") ? ALL_WHITE : entryColor;
+		m_colors[1] = (infoColor == "\0") ? ALL_GREEN : infoColor;
+		m_colors[2] = (warningColor == "\0") ? ALL_YELLOW : warningColor;
+		m_colors[3] = (errorColor == "\0") ? ALL_RED : errorColor;
 	}
-	template<typename ...Types>
-	void info(Types&& ... args)
-	{
-
-	}
-	template<typename ...Types>
-	void warning(Types&& ... args)
-	{
-
-	}
-	template<typename ...Types>
-	void error(Types&& ... args)
-	{
-
-	}
-
-
 private:
-	Logger() {}
+	Logger() :m_timeStart(std::chrono::high_resolution_clock::now()) {}
+	const std::chrono::steady_clock::time_point m_timeStart;
+	const std::string m_type[3] = { "[ENT] ", "[INF] ", "[WAR] "};
+	std::string m_colors[4] = { ALL_WHITE, ALL_GREEN, ALL_YELLOW, ALL_RED};
+	std::stringstream m_msg;
+
+	std::string getTime()
+	{
+		int time = static_cast<int>(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - m_timeStart).count());
+		int hours = time / 3600;
+		int minutes = (time - hours*3600) / 60;
+		int seconds = time - hours *3600 - minutes * 60;
+		std::stringstream result;
+		result << "[" << std::setfill('0') << std::setw(2) << std::to_string(hours) << ":" << std::setw(2) << std::to_string(minutes) << ":" << std::setw(2) << std::to_string(seconds) << "]";
+		return result.str();
+	}
+
+	size_t findToken(const std::string& text, size_t* tlc0, size_t* tlc1)
+	{
+		size_t p0 = text.find("{");
+		if (text.find("}") - p0 == 2)
+		{ return p0; }
+		else
+		{ return std::string::npos; }
+	}
+	template<typename T>
+	void proccesToken(char token, T arg)
+	{
+		m_msg << arg;
+	}
+	//===============================================================================
+	// recursive output
+	//===============================================================================
+	void output(const std::string& text)
+	{ m_msg << text; }
+	template<typename T, typename ...Types>
+	void output(const std::string& text, T&& arg, Types&& ...args)
+	{
+		const size_t tlc0 = text.find("{");
+		const size_t tlc1 = text.find("}");
+		if (tlc0 == std::string::npos) { m_msg << text; }
+		else if (tlc1 - tlc0 != 2)
+		{
+			m_msg << text.substr(0, tlc1 + 1);
+			output(text.substr(tlc1 + 2), arg, args...);
+		}
+		else
+		{
+			m_msg << text.substr(0, tlc0);
+			proccesToken(text[tlc0 + 1], arg);
+			output(text.substr(tlc0 + 3), args...);
+		}
+	}
 
 };
 }
--- ALL/ALL/test.cpp
+++ ALL/ALL/test.cpp
@@ -1,10 +1,11 @@
 #include"all.hpp"
+#include<ctime>
 
 int main()
 {
-	
-	ALL_ENT("test", 12, 18);
+	ALL_ENT("dupa {00}, {0}, { }, {00}, { } haha {0}", 2, 3, 4, 5);
 
 
+	system("PAUSE");
 	return 0;
 }
