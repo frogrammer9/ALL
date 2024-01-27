32fc5eb0dc3ff370f998e9e0216453cc2e3523dc
frogrammer9 <oskar@meler.pl>
2024-01-27 22:37:48 +0100

    Fixed issue #1 ... Improved the bad code mentioned in the last commit Added the posibility to log to a file Got rid of unwanted behaviour

--- ALL/ALL/all.hpp
+++ ALL/ALL/all.hpp
@@ -4,7 +4,6 @@
 #include<iostream>
 #include<iomanip>
 #include<sstream>
-#include<string>
 #include<fstream>
 #include<chrono>
 
@@ -74,13 +73,23 @@
 #define ALL_WHITE			"\033[1;37m"
 #define ALL_DEFAULT			"\0"
 
-typedef std::string all_Color;
+#define ALL_LOG_MODE_ENT	0
+#define ALL_LOG_MODE_INF	1
+#define ALL_LOG_MODE_WAR	2
+#define ALL_LOG_MODE_ERR	3
+
+typedef std::string all_color;
 
 namespace all {
 class Logger
 {
 public:
-	~Logger() = default;
+	~Logger() {
+		if (m_toFile) {
+			std::ofstream out(m_filepath ,std::ios::out);
+			out << m_msg.str(); out.close();
+		}
+	}
 	Logger(const Logger&) = delete;
 	Logger& operator = (const Logger&) = delete;
 	static Logger& getInstance()
@@ -91,29 +100,41 @@ public:
 	template<typename T, typename ...Types>
 	void log(uint8_t mode, T first, Types&& ... args)
 	{
-		m_msg = std::stringstream();
-		m_msg << m_colors[mode];
+		if (!m_toFile) {
+			m_msg = std::stringstream();
+			m_msg << m_colors[mode];
+		}
 		m_msg << getTime() << m_type[mode];
 		output(first, args...);
-		m_msg << "\033[0m"; std::cout << m_msg.str() << "\n";
+		m_msg << '\n';
+		if (!m_toFile) { m_msg << "\033[0m"; std::cout << m_msg.str(); }
 	}
 	//===============================================================================
 	// settings
 	//===============================================================================
-	void setColors(all_Color entryColor, all_Color infoColor, all_Color warningColor, all_Color errorColor)
+	void setColors(all_color entryColor, all_color infoColor, all_color warningColor, all_color errorColor)
 	{
 		m_colors[0] = (entryColor == "\0") ? ALL_WHITE : entryColor;
 		m_colors[1] = (infoColor == "\0") ? ALL_GREEN : infoColor;
 		m_colors[2] = (warningColor == "\0") ? ALL_YELLOW : warningColor;
 		m_colors[3] = (errorColor == "\0") ? ALL_RED : errorColor;
 	}
+	void setFilePath(const std::string& filePath) 
+	{ m_filepath = filePath; m_toFile = true; }
+	void setFilePath(const char* filePath)
+	{ m_filepath = filePath; m_toFile = true; }
 private:
 	Logger() :m_timeStart(std::chrono::high_resolution_clock::now()) {}
 	const std::chrono::steady_clock::time_point m_timeStart;
+	bool m_toFile = false;
+	std::string m_filepath;
 	const std::string m_type[4] = { "[ENT] ", "[INF] ", "[WAR] ", "[ERR] "};
 	std::string m_colors[4] = { ALL_WHITE, ALL_GREEN, ALL_YELLOW, ALL_RED};
 	std::stringstream m_msg;
 
+	//===============================================================================
+	// helper funcions
+	//===============================================================================
 	std::string getTime()
 	{
 		int time = static_cast<int>(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - m_timeStart).count());
@@ -124,54 +145,47 @@ private:
 		result << "[" << std::setfill('0') << std::setw(2) << std::to_string(hours) << ":" << std::setw(2) << std::to_string(minutes) << ":" << std::setw(2) << std::to_string(seconds) << "]";
 		return result.str();
 	}
+	size_t findToken(const char* string) const
+	{
+		if (string[0] == '\0') { return std::string::npos; }
+		if (string[1] == '\0') { return std::string::npos; }
+		size_t i = 0;
+		while (string[i + 2] != '\0')
+		{
+			if (string[i] == '{' && string[i + 2] == '}') { return i; }
+			i++;
+		}
+		return std::string::npos;
+	}
 	//===============================================================================
 	// recursive output
 	//===============================================================================
 	template<typename T>
 	void output(T arg)
 	{ m_msg << arg; }
+	template<typename T>
+	void outputnostr(T arg)
+	{ m_msg << arg; }
 	template<typename T, typename ...Types>
-	void output(const char* t, T&& arg, Types&& ...args)			//TODO Rethink and improve these two funcions. It works for now but it is disgusting!!!
+	void output(const char* text, T&& arg, Types&& ...args)
 	{
-		const std::string text(t);
-		const size_t tlc0 = text.find("{");
-		const size_t tlc1 = text.find("}");
-		if (tlc0 == std::string::npos) { m_msg << text; }
-		else if (tlc1 - tlc0 != 2)
-		{
-			m_msg << text.substr(0, tlc1 + 1);
-			output(text.substr(tlc1 + 2), arg, args...);
-		}
-		else
-		{
-			m_msg << text.substr(0, tlc0);
-			proccesToken(text[tlc0 + 1], arg);
-			output(text.substr(tlc0 + 3), args...);
-		}
-	}
-	template<typename T, typename ...Types>
-	void output(std::string text, T&& arg, Types&& ...args)			//TODO Rethink and improve these two funcions. It works for now but it is disgusting!!!
-	{
-		const size_t tlc0 = text.find("{");
-		const size_t tlc1 = text.find("}");
-		if (tlc0 == std::string::npos) { m_msg << text; }
-		else if (tlc1 - tlc0 != 2)
-		{
-			m_msg << text.substr(0, tlc1 + 1);
-			output(text.substr(tlc1 + 2), arg, args...);
-		}
-		else
-		{
-			m_msg << text.substr(0, tlc0);
-			proccesToken(text[tlc0 + 1], arg);
-			output(text.substr(tlc0 + 3), args...);
-		}
+		const size_t tlc = findToken(text);
+		if (tlc == std::string::npos) { m_msg << text; return; }
+		m_msg.write(text, tlc);	
+		proccesToken(text[tlc + 1], arg);
+		output(&text[tlc+3], args...);
 	}
 	template<typename T, typename ...Types>
 	void output(T&& arg, Types&& ...args)
 	{
 		m_msg << arg << ", ";
-		output(args...);
+		outputnostr(args...);
+	}
+	template<typename T, typename ...Types>
+	void outputnostr(T&& arg, Types&& ...args)
+	{
+		m_msg << arg << ", ";
+		outputnostr(args...);
 	}
 	//===============================================================================
 	// tokens processing
@@ -181,7 +195,6 @@ private:
 	{
 		m_msg << arg;
 	}
-
 };
 }
 #endif
--- ALL/ALL/test.cpp
+++ ALL/ALL/test.cpp
@@ -3,16 +3,14 @@
 
 
 
-
-
 int main()
 {
+	//ALL_LOG.setFilePath("log.txt"); example
 	ALL_LOG.setColors(ALL_DEFAULT, ALL_BLUE, ALL_DEFAULT, ALL_DEFAULT);
-
-	ALL_ENT("test {00}, {0}, { , {0}, { } haha {0}", 2, 3, 4, 5);
+	std::string text = "test {0}";
+	ALL_ENT(12, 69, "test {00}, {0}, { , {0}, { } haha {0}", 2, 3, 5, 6, 7, 8);
+	ALL_INF(text, 12);
 	ALL_ENT(2, 3, 4, 5);
-
-
 
 	system("PAUSE");
 	return 0;
